b9352297ad2bcc46c16d9d7baab7fb635c83f526
davwilliams <dwilliams@williamsgarcia.com>
2017-06-30 03:15:19 -0400

    Update logstash and zookeeper packages

--- packages/zookeeper-3.4.10_start-zk.sh
+++ packages/zookeeper-3.4.10_start-zk.sh
@@ -7,8 +7,7 @@ sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
 # Configure clustering
 if [ $ZK_CLUSTERED = true ]; then
   node=`echo -n $CNTM_JOB_FQN | cut -d: -f 5`
-  node_num=${$node: -1}
-  echo $node_num > $ZK_HOME/data/myid
+  echo $ZK_NODE_ID > $ZK_HOME/data/myid
   for x in `seq ${ZK_CLUSTER_SIZE}`; do
     echo "server.${x}=zookeeper-node${x}.apcera.local:2888:3888" >> $ZK_HOME/conf/zoo.cfg
   done
